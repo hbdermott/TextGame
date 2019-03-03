f44eefec97d0d7a3c7e76b6adeae31537815748a
Hunter Dermott <hbdermott@gmail.com>
2019-03-02 18:58:29 -0800

    Bad stuff but its progress my man

--- app/src/main/java/com/example/practice/choice_1.java
+++ app/src/main/java/com/example/practice/choice_1.java
@@ -1,5 +1,6 @@
 package com.example.practice;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,5 +13,15 @@ public class choice_1 extends com.example.practice.Template {
         setContentView(R.layout.activity_choice_1);
         findViews((ViewGroup) findViewById(R.id.layoutView));
         tapHelper();
+    }
+    public void screenTapped(View view)
+    {
+        tapHelper();
+    }
+
+    public void girlorBoy(View view)
+    {
+        Intent intent = new Intent(this, boyvsgirl.class );
+        startActivity(intent);
     }
 }
