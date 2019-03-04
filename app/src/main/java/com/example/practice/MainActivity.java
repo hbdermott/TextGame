78f7fc342d09b2041bcba240895e94017b71bb7e
Hunter Dermott <hbdermott@gmail.com>
2019-03-04 00:32:06 -0800

    Slight Size reduction and bug fixes

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.practice;
 
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-
-import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity {
